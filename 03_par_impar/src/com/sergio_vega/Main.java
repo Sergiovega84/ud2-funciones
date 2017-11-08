abae24f83b1f8e81c47db02fee2512cf98fb7b90
Sergio Vega <lakio84@gmail.com>
2017-11-08 19:11:13 +0100

    soluciones

---
+++ 03_par_impar/src/com/sergio_vega/Main.java
@@ -0,0 +1,30 @@
+package com.sergio_vega;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int espar(int a) {
+        if (a % 2 == 0) {
+            System.out.println("el número es par ");
+        }
+        ;
+        return a;
+    }
+
+    public static int esimpar(int a) {
+        if (a % 2 != 0) {
+            System.out.println("el número es impar ");
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("introduce número: ");
+        int a = Integer.parseInt(br.readLine());
+        a = espar(a);
+        a = esimpar(a);
+    }
+}
